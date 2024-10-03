5aec8297bbe779c56c3c788108c96d507ea303f1
luyenit <luyentvph41936@fpt.edu.vn>
2024-10-03 11:23:28 +0700

    code đầy đủ admin

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit050656eddcb15558a54c285f9dfb9479
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit050656eddcb15558a54c285f9dfb9479::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire050656eddcb15558a54c285f9dfb9479($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit050656eddcb15558a54c285f9dfb9479::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire050656eddcb15558a54c285f9dfb9479($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }

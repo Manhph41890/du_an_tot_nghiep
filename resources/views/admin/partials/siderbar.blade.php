779790ed65531e867c1192c0d47f44852bc6522f
Mr Toan <toanhxph40144@fpt.edu.vn>
2024-10-07 14:57:13 +0700

    07/10

--- resources/views/admin/partials/siderbar.blade.php
+++ resources/views/admin/partials/siderbar.blade.php
@@ -287,6 +287,21 @@
                     <div class="collapse" id="baiviet">
                         <ul class="nav-second-level">
                             <li>
+                                {{-- <a class='tp-link' href='{{ route('danhgia.index') }}'>Danh sách</a> --}}
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#danhgia" data-bs-toggle="collapse">
+                        <i data-feather="file-text"></i>
+                        <span> Đánh giá </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="danhgia">
+                        <ul class="nav-second-level">
+                            <li>
                                 <a class='tp-link' href='{{ route('danhgia.index') }}'>Danh sách</a>
                             </li>
                         </ul>

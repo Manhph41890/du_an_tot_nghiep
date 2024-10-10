408253204dffe3fd6e61dcbc11d0ce646143db65
AnhDuc004 <ducanh29304@gmail.com>
2024-10-10 23:56:37 +0700

    fix3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel1</title>
+    <title>Laravel</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -878,9 +878,10 @@
                             <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>
 
                             <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Laravel has wonderful documentation covering every aspect of the framework. Whether you
-                                are a newcomer or have prior experience with Laravel, we recommend reading our
-                                documentation from beginning to end.
+                                Laravel has wond1erful document1ation covering every aspect of the framework. Whether
+                                you
+                                are a newcom1er or have prior experience with Laravel, we recommend reading our
+                                documentat1ion from beginning to end.
                             </p>
                         </div>
 
@@ -906,9 +907,9 @@
                             <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laracasts</h2>
 
                             <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Laracasts offers1 thousands of video tutorials on Laravel, PHP, and JavaScript
-                                development. Check them out, see for yourself, and massively level up your development
-                                skills in the process.
+                                Laracasts offers11 thousands of video tutorials on Laravel, PHP, and JavaScript
+                                development. Check1 them out, see for yourself, and massively level up your development
+                                skills in the pr1ocess.
                             </p>
                         </div>
 

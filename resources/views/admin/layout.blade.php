8261e2fab1de16d02c1214ea615d1ab04743f4f5
Manhph41890 <fpldev7@gmail.com>
2024-10-22 00:17:31 +0700

    main

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -9,7 +9,7 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>{{$title}}</title>
+    {{-- <title>{{$title}}</title> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc." />
     <meta name="author" content="Zoyothemes" />

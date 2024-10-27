d8243687f740ac4153ff877ce32c217639c21c15
luyenit <luyentvph41936@fpt.edu.vn>
2024-10-27 15:30:59 +0700

    update lại chức vụ full

--- resources/views/admin/chucvu/create.blade.php
+++ resources/views/admin/chucvu/create.blade.php
@@ -5,23 +5,16 @@
 
 @section('content')
     <div class="content-page">
-
         <div class="content">
-
-            <!-- Start Content-->
             <div class="container-xxl">
                 <div class="py-3 d-flex align-items-sm-center flex-sm-row flex-column">
-                    <div class="flex-grow-1">
-                        <h4 class="fs-18 fw-semibold m-0"> {{ $title }} </h4>
-                    </div>
                 </div>
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h5 class="card-title mb-0"></h5>
-                            </div><!-- end card header -->
-
+                                <h5 class="card-title mb-0">{{ $title }}</h5>
+                            </div>
                             <div class="card-body">
                                 <form action="{{ route('chucvus.store') }}" method="POST">
                                     @csrf
@@ -29,7 +22,7 @@
                                         <div class="col-lg-6">
                                             <div class="mb-3">
                                                 <label for="ten_chuc_vu" class="form-label">Tên chức vụ</label>
-                                                <input type="text" id="ten_chuc_vu" name="ten_chuc_vu"
+                                                <input type="text" id="ten_chuc_vu" name="ten_chuc_vu" placeholder="Ví dụ: nhan_vien,khach_hang,.."
                                                     class="form-control @error('ten_chuc_vu') is-invalid @enderror"
                                                     value="{{ old('ten_chuc_vu') }}">
                                                 @error('ten_chuc_vu')
@@ -39,7 +32,6 @@
                                                 @enderror
                                             </div>
                                         </div>
-
                                         <div class="col-lg-6">
                                             <div class="mb-3">
                                                 <label for="mo_ta_chuc_vu" class="form-label">Mô tả</label>
@@ -53,23 +45,16 @@
                                                 @enderror
                                             </div>
                                         </div>
-
                                         <div class="d-flex justify-content-center">
                                             <button type="submit" class="btn btn-success">Thêm mới</button>
                                         </div>
-
-
                                     </div>
                                 </form>
                             </div>
-
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div> <!-- container-fluid -->
-@endsection
-
-@section('js')
 @endsection

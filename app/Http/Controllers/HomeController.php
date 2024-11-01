fe9354047d66356c45699eee5b64ed06c1713655
Manhph41890 <fpldev7@gmail.com>
2024-11-01 22:22:27 +0700

    sanpham lien quan

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -63,9 +63,10 @@ class HomeController extends Controller
 
         $sizes = $sanPhamCT->bien_the_san_phams->pluck('size')->unique('id'); // Lấy size duy nhất
         $colors = $sanPhamCT->bien_the_san_phams->pluck('color')->unique('id'); // Lấy màu sắc duy nhất
+        $sanLienQuan = san_pham::with('danh_muc')->where('id', '!=', $id)->orderByDesc('id')->get();
         $title = "";
 
-        return view('client.sanpham.sanphamct', compact('sanPhamCT', 'sizes', 'colors', 'title', 'uniqueColors'));
+        return view('client.sanpham.sanphamct', compact('sanPhamCT', 'sizes', 'colors', 'title', 'uniqueColors', 'sanLienQuan'));
     }
 
     // In Data Bai viet

d3180d80a50d75d0a0de81c10d79b59a6e5d496d
Mr Toan <toanhxph40144@fpt.edu.vn>
2024-11-08 13:47:19 +0700

    toi uu code

--- resources/views/admin/danhgia/index.blade.php
+++ resources/views/admin/danhgia/index.blade.php
@@ -102,10 +102,6 @@
                                                         <div class="modal fade" id="myModal{{ $item->id }}" tabindex="-1" aria-labelledby="exampleModalLabel{{ $item->id }}" aria-hidden="true">
                                                             <div class="modal-dialog">
                                                                 <div class="modal-content">
-                                                                    {{-- <div class="modal-header">
-                                                                        <h5 class="modal-title" id="exampleModalLabel{{ $item->id }}">Chi tiết đánh giá</h5>
-                                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                                    </div> --}}
                                                                     <div class="modal-body">
                                                                         @include('admin.danhgia.show', ['post' => $item])
                                                                     </div>

7d79d61917409fceafbbba57405073bf7919d0c2
Manhph41890 <fpldev7@gmail.com>
2024-11-09 12:07:15 +0700

    .hh

--- routes/web.php
+++ routes/web.php
@@ -139,10 +139,9 @@ Route::middleware(['auth', 'role:khach_hang'])->group(function () {
 
     Route::post('/order/add', [OrderController::class, 'add'])->name('order.add');
     Route::get('/order/success', [OrderController::class, 'success'])->name('order.success');
+    Route::get('/order/success_nhanhang', [OrderController::class, 'success_nhanhang'])->name('order.success_nhanhang');
 
     Route::post('/apply-coupon', [OrderController::class, 'applyCoupon'])->name('apply.coupon');
-
-
 
     // 
 });

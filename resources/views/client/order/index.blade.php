a84133a0812c9c7ff83271a5adda5326e92bb6d0
Manhph41890 <fpldev7@gmail.com>
2024-11-09 10:19:00 +0700

    done

--- resources/views/client/order/index.blade.php
+++ resources/views/client/order/index.blade.php
@@ -23,164 +23,189 @@
     <!-- product tab start -->
     <section class="whish-list-section theme1 pt-80 pb-80">
         <div class="container">
-            <div class="page_header">
-                <h1>Sign In or Create an Account</h1>
-            </div>
-            <table class="table table-striped cart-list">
-                <thead>
-                    <tr>
-                        <th class="text-center">Ảnh sản phẩm</th>
-                        <th class="text-center">Tên sản phẩm</th>
-                        <th class="text-center">Biến thể</th>
-                        <th class="text-center">Số lượng</th>
-                        <th class="text-center">Giá</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($cart->cartItems as $item)
-                        <tr>
-                            <td class="text-center"><img scope="row"
-                                    src="{{ asset('/storage/' . $item->san_pham->anh_san_pham) }}" alt=" anh_san_pham"
-                                    style="width: 50" height="50"></td>
-                            <td class="text-center">{{ $item->san_pham->ten_san_pham }}</td>
-                            <td class="text-center">
-                                @if ($item->size && $item->color)
-                                    <span>Size: {{ $item->size->ten_size }}
-                                    </span>
-                                    <br>
-                                    <span class="">
-                                        Color: {{ $item->color->ten_color }}</span>
-                                @else
-                                    <span>Không có thông tin kích thước hoặc màu sắc</span>
-                                @endif
-                            </td>
-                            <td class="text-center">{{ $item->quantity }}</td>
-                            <td class="text-center">${{ $item->price }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
-            <!-- /page_header -->
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="step first">
-                        <h3>1. User Info and Billing address</h3>
-
-                        <div class="tab-content checkout">
-                            <div class="tab-pane fade show active" id="tab_1" role="tabpanel" aria-labelledby="tab_1">
-                                <label>Tên</label>
-                                <input type="text" name="ho_ten" class="form-control" required
-                                    value="{{ old('ho_ten', Auth::user()->ho_ten ?? '') }}" />
-                                @error('ho_ten')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                                <label>Số điện thoại</label>
-                                <input type="text" name="so_dien_thoai" class="form-control" required
-                                    value="{{ old('so_dien_thoai', Auth::user()->so_dien_thoai ?? '') }}" />
-                                @error('so_dien_thoai')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                                <label>Email</label>
-                                <input type="email" name="email" class="form-control" required
-                                    value="{{ old('email', Auth::user()->email ?? '') }}" />
-                                @error('email')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                                <label>Địa chỉ</label>
-                                <input type="text" name="dia_chi" class="form-control" required
-                                    value="{{ old('dia_chi', Auth::user()->dia_chi ?? '') }}" />
-                                @error('dia_chi')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </div>
-                        </div>
-                    </div>
-                    <!-- /step -->
+            <form action="{{ route('order.add') }}" class="personal-information" method="POST">
+                @csrf
+                <div class="page_header">
+                    <h1>Thông tin đơn hàng</h1>
                 </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="step middle payments">
-                        <h3>2. Payment and Shipping</h3>
-                        <ul>
-                            @foreach ($phuongThucThanhToans as $phuongThucThanhToan)
-                                <li>
-                                    <label class="container_radio">
-                                        {{ $phuongThucThanhToan->kieu_thanh_toan }}
-                                        <a href="#0" class="info" data-bs-toggle="modal"
-                                            data-bs-target="#payments_method"></a>
-                                        <input type="radio" id="payment-{{ $phuongThucThanhToan->id }}"
-                                            name="phuong_thuc_thanh_toan" class="form-check-input"
-                                            value="{{ $phuongThucThanhToan->id }}"
-                                            {{ old('phuong_thuc_thanh_toan') == $phuongThucThanhToan->id ? 'checked' : '' }}
-                                            onchange="toggleOrderButton('{{ $phuongThucThanhToan->kieu_thanh_toan }}')">
-                                        <span class="checkmark"></span>
-                                    </label>
-                                    {{-- Hiển thị logo VNPay nếu kieu_thanh_toan là "Thanh toán online" --}}
-                                    @if ($phuongThucThanhToan->kieu_thanh_toan == 'Thanh toán online')
-                                        <div class="vnpay-logo ms-2">
-                                            <img src="/assets/client/img/logo/logo-vector-vi-vnpay-mien-phi.png"
-                                                alt="VNPay Logo">
-                                        </div>
+                <table class="table table-hover cart-list">
+                    <thead class="table-light custom-thead">
+                        <tr>
+                            <th class="text-center" style="width: 15%;">Ảnh sản phẩm</th>
+                            <th class="text-center" style="width: 30%;">Tên sản phẩm</th>
+                            <th class="text-center" style="width: 20%;">Biến thể</th>
+                            <th class="text-center" style="width: 10%;">Số lượng</th>
+                            <th class="text-center" style="width: 15%;">Giá</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($cart->cartItems as $item)
+                            <tr>
+                                <td class="text-center">
+                                    <img src="{{ asset('/storage/' . $item->san_pham->anh_san_pham) }}" alt="Ảnh sản phẩm"
+                                        class="img-fluid" style="max-width: 80px; max-height: 80px;">
+                                </td>
+                                <td class="text-center">{{ $item->san_pham->ten_san_pham }}</td>
+                                <td class="text-center">
+                                    @if ($item->size && $item->color)
+                                        <span>Size: {{ $item->size->ten_size }}</span>
+                                        <br>
+                                        <span>Color: {{ $item->color->ten_color }}</span>
+                                    @else
+                                        <span class="text-muted">Không có thông tin kích thước hoặc màu sắc</span>
                                     @endif
-                                </li>
-                            @endforeach
-                        </ul>
-                        <div class="">
-                            <div class="step">
-                                <h3>Nhập mã giảm giá để nhận ưu đãi</h3>
-                            </div>
-                            <div class="d-flex">
-                                <input type="text" id="coupon-code" name="khuyen_mai" class="form-control"
-                                    placeholder="Nhập mã giảm giá">
-                                <div class="input-group-append">
-                                    <button class="btn btn-outline-success ms-3" id="apply-coupon" type="">Áp
-                                        dụng</button>
+                                </td>
+                                <td class="text-center">{{ $item->quantity }}</td>
+                                <td class="text-center"><strong>${{ number_format($item->price, 2) }}</strong></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+                <!-- /page_header -->
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="step first">
+                            <h3>1. Thông tin nhận hàng</h3>
+
+                            <div class="tab-content checkout">
+                                <div class="tab-pane fade show active" id="tab_1" role="tabpanel"
+                                    aria-labelledby="tab_1">
+                                    <label>Tên</label>
+                                    <input type="text" name="ho_ten" class="form-control" required
+                                        value="{{ old('ho_ten', Auth::user()->ho_ten ?? '') }}" />
+                                    @error('ho_ten')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <label>Số điện thoại</label>
+                                    <input type="text" name="so_dien_thoai" class="form-control" required
+                                        value="{{ old('so_dien_thoai', Auth::user()->so_dien_thoai ?? '') }}" />
+                                    @error('so_dien_thoai')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <label>Email</label>
+                                    <input type="email" name="email" class="form-control" required
+                                        value="{{ old('email', Auth::user()->email ?? '') }}" />
+                                    @error('email')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <label>Địa chỉ</label>
+                                    <input type="text" name="dia_chi" class="form-control" required
+                                        value="{{ old('dia_chi', Auth::user()->dia_chi ?? '') }}" />
+                                    @error('dia_chi')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
-
+                        <!-- /step -->
                     </div>
-                    <!-- /step -->
-
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="step last">
-                        <h3>3. Order Summary</h3>
-                        <div class="box_general summary">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="step middle payments">
+                            <h3>2. Thông tin thanh toán</h3>
                             <ul>
-                                <li class="clearfix"><em><strong>Tiền sản phẩm</strong></em>
-                                    <span>{{ $total }}</span>
-                                </li>
+                                @foreach ($phuongThucThanhToans as $phuongThucThanhToan)
+                                    <li>
+                                        <label class="container_radio">
+                                            {{ $phuongThucThanhToan->kieu_thanh_toan }}
+                                            <a href="#0" class="info" data-bs-toggle="modal"
+                                                data-bs-target="#payments_method"></a>
+                                            <input type="radio" id="payment-{{ $phuongThucThanhToan->id }}"
+                                                name="phuong_thuc_thanh_toan" class="form-check-input"
+                                                value="{{ $phuongThucThanhToan->id }}"
+                                                {{ old('phuong_thuc_thanh_toan') == $phuongThucThanhToan->id ? 'checked' : '' }}
+                                                onchange="toggleOrderButton('{{ $phuongThucThanhToan->kieu_thanh_toan }}')">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                        {{-- Hiển thị logo VNPay nếu kieu_thanh_toan là "Thanh toán online" --}}
+                                        @if ($phuongThucThanhToan->kieu_thanh_toan == 'Thanh toán online')
+                                            <div class="vnpay-logo ms-2">
+                                                <img src="/assets/client/img/logo/logo-vector-vi-vnpay-mien-phi.png"
+                                                    alt="VNPay Logo">
+                                            </div>
+                                        @endif
+                                    </li>
+                                @endforeach
                             </ul>
-                            <ul>
-                                <li class="clearfix"><em><strong>Tiền vận chuyển</strong></em> <span> 30.000</span></li>
-                                <li class="clearfix"><em><strong>Tiền giảm giá khuyến mại</strong></em> <span
-                                        id="total-price-display"></span></li>
-                            </ul>
-
-                            <div class="total clearfix">Tổng cộng <span id="total-price-display">{{ $totall }}</span>
+                            <div class="">
+                                <div class="step">
+                                    <h3>Nhập mã giảm giá để nhận ưu đãi</h3>
+                                </div>
+                                <div class="row">
+                                    <div class="input-group">
+                                        <input type="text" id="coupon-code" name="khuyen_mai" class="form-control"
+                                            placeholder="Nhập mã giảm giá">
+                                        <button class="btn btn-outline-success" id="apply-coupon" type="button">Áp
+                                            dụng</button>
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="place-order mt-10" id="place-order-cod" style="display: none;">
-                                <button type="submit" class="btn_1 full-width btn-block btn-primary">Đặt Hàng</button>
-                            </div>
-                            <div class="place-order mt-10" id="place-order-online" style="display: none;">
-                                <button type="submit" class="btn_1 full-width btn-block btn-danger"
-                                    name="redirect">Thanh
-                                    toán ngay</button>
-                            </div>
                         </div>
-                        <!-- /box_general -->
+                        <!-- /step -->
+
                     </div>
-                    <!-- /step -->
+                    <div class="col-lg-4 col-md-6">
+                        <div class="step last">
+                            <h3>3. Thanh toán</h3>
+                            <div class="box_general summary">
+                                <ul>
+                                    <li class="clearfix"><em><strong>Tiền sản phẩm</strong></em>
+                                        <span>{{ $total }}</span>
+                                    </li>
+                                </ul>
+                                <ul>
+                                    <li class="clearfix"><em><strong>Tiền vận chuyển</strong></em> <span> 30.000</span>
+                                    </li>
+                                    <li class="clearfix"><em><strong>Tiền giảm giá khuyến mại</strong></em> <span
+                                            id="discount-amount">0₫</span></li>
+                                </ul>
+
+                                <div class="total clearfix">Tổng cộng <span id="total_amount"
+                                        data-total="{{ $totall }}">{{ number_format($totall, 2) }}₫</span>
+                                </div>
+
+                                <div class="place-order mt-10" id="place-order-cod" style="display: none;">
+                                    <button type="submit" class="btn_1 full-width btn-block btn-primary">Đặt
+                                        Hàng</button>
+                                </div>
+                                <div class="place-order mt-10" id="place-order-online" style="display: none;">
+                                    <button type="submit" class="btn_1 full-width btn-block btn-danger"
+                                        name="redirect">Thanh
+                                        toán ngay</button>
+                                </div>
+                            </div>
+                            <!-- /box_general -->
+                        </div>
+                        <!-- /step -->
+                    </div>
                 </div>
-            </div>
-            <!-- /row -->
+                <!-- /row -->
+            </form>
         </div>
         <style>
             .vnpay-logo {
                 width: 80px;
                 display: block;
+            }
+
+            .custom-thead {
+                background-color: #333 !important;
+                /* Màu nền */
+            }
+
+            /* Thay đổi màu chữ của các thẻ <th> */
+            .custom-thead th {
+                color: white !important;
+                /* Màu chữ */
+            }
+
+            /* Nếu bạn muốn thay đổi cả màu nền cho các ô thẻ <th> */
+            .custom-thead th {
+                background-color: #333 !important;
+                /* Nền của các thẻ <th> */
+                color: #fff !important;
+                /* Màu chữ của các thẻ <th> */
             }
         </style>
         <script>

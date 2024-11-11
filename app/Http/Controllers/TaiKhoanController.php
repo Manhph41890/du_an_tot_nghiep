4708a63f6330ecebf086158454ae96bd8322398f
canmuidz <150830621+canmuidz@users.noreply.github.com>
2024-11-11 13:59:55 +0700

    .

--- app/Http/Controllers/TaiKhoanController.php
+++ app/Http/Controllers/TaiKhoanController.php
@@ -82,69 +82,6 @@ class TaiKhoanController extends Controller
             }
         }
     }
-    //set
-    // public function updateThongtin(Request $request)
-    // {
-    //     // Get the currently authenticated user
-    //     $user = Auth::user();
-
-    //     // Validate the input data
-    //     $request->validate([
-    //         'ho_ten' => 'required|string|max:255',
-    //         'email' => 'required|email|unique:users,email,' . $user->id,
-    //         'so_dien_thoai' => 'nullable|string|max:15',
-    //         'ngay_sinh' => 'nullable|date',
-    //         'gioi_tinh' => 'nullable|string|max:10',
-    //         'dia_chi' => 'nullable|string|max:255',
-    //     ]);
-
-    //     // xu ly anh
-    //     if ($request->isMethod('post')) {
-    //         $request->validate([
-    //             'anh_dai_dien' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-    //         ]);
-
-    //         $params = $request->except('_token');
-    //         $user = Auth::user();
-
-    //         if ($request->hasFile('anh_dai_dien')) {
-    //             // Delete old avatar
-    //             if ($user->anh_dai_dien) {
-    //                 Storage::disk('public')->delete($user->anh_dai_dien);
-    //             }
-
-    //             // Save new avatar
-    //             $filepath = $request->file('anh_dai_dien')->store('uploads/avatars', 'public');
-    //             $params['anh_dai_dien'] = $filepath;
-    //         } else {
-    //             $params['anh_dai_dien'] = $user->anh_dai_dien;
-    //         }
-
-    //         try {
-    //             // Update user profile
-    //             $user->update($params);
-
-    //             return redirect()->route('taikhoan.dashboard')->with('success', 'Cập nhật hình đại diện thành công');
-    //         } catch (\Exception $e) {
-    //             return redirect()->back()->with('error', 'Có lỗi xảy ra: ' . $e->getMessage());
-    //         }
-    //     }
-
-    //     // Update the user's information
-    //     $user->ho_ten = $request->input('ho_ten');
-    //     $user->email = $request->input('email');
-    //     $user->so_dien_thoai = $request->input('so_dien_thoai');
-    //     $user->ngay_sinh = $request->input('ngay_sinh');
-    //     $user->gioi_tinh = $request->input('gioi_tinh');
-    //     $user->dia_chi = $request->input('dia_chi');
-
-    //     // Save the updated user information
-    //     $user->save();
-
-    //     // Redirect back with dddda success message
-
-    //     return redirect()->route('taikhoan.dashboard')->with('success', 'Thông tin tài khoản đã được cập nhật thành công.');
-    // }
 
     public function updateThongtin(Request $request)
 {

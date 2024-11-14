3ce8e192c918d0051a1dfef1bbb6a7bd96452a14
Manhph41890 <fpldev7@gmail.com>
2024-11-14 17:54:54 +0700

    .

---
+++ app/Models/huy_don_hang.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class huy_don_hang extends Model
+{
+    use HasFactory;
+}

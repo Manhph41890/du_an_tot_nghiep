37452410750b308168ddda78facf942d8aecf1e1
luyenit <luyentvph41936@fpt.edu.vn>
2024-11-16 21:48:02 +0700

    suwar home

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -310,41 +310,17 @@
     <!-- product tab start -->
     <section class="product-tab bg-white pt-50 pb-80">
         <div class="container">
-            <div class="product-tab-nav mb-50">
-                <div class="row align-items-center">
-                    {{-- <div class="col-12">
-                        <div class="section-title text-center">
-                            <h2 class="title pb-3 mb-3">Sản phẩm của chúng tôi</h2>
-                            <p class="text">Vẽ sáng tạo - Tô hạnh phúc</p>
-                        </div>
-                    </div> --}}
-                    <div class="col-12">
-                        <nav class="product-tab-menu theme1">
-                            <ul class="nav nav-pills justify-content-center" id="pills-tab" role="tablist">
-                                <li class="nav-item">
-                                    <a class="nav-link active" id="pills-home-tab" data-bs-toggle="pill"
-                                        href="#pills-home" role="tab" aria-controls="pills-home"
-                                        aria-selected="true" style="font-size: 20px">Sản phẩm mới</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" id="pills-profile-tab" data-bs-toggle="pill"
-                                        href="#pills-profile" role="tab" aria-controls="pills-profile"
-                                        aria-selected="false" style="font-size: 20px">Đang giảm giá</a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            </div>
             <div class="row">
                 <div class="col-12">
                     <div class="tab-content" id="pills-tabContent">
                         <!-- first tab-pane -->
-                        <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
-                            aria-labelledby="pills-home-tab">
+                        <div class="pb-80">
                             <div class="row">
+                                <div class="section-title text-center">
+                                    <h2 class="title pb-3 mb-3">Sản phẩm Mới</h2>
+                                </div>
                                 @foreach ($sanPhamMois as $item)
-                                    <div class="col-12 col-md-4 col-lg-2 mb-4">
+                                    <div class="col-12 col-md-6  col-lg-2 mb-4">
                                         <div class="card product-card">
                                             <div class="card-body p-0">
                                                 <div class="media flex-column">
@@ -392,8 +368,11 @@
                             </div>
                         </div>
                         <!-- second tab-pane -->
-                        <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-home-tab">
+                        <div class="pb-80" >
                             <div class="row">
+                                <div class="section-title text-center">
+                                    <h2 class="title pb-3 mb-3">Sản phẩm giảm giá</h2>
+                                </div>
                                 @foreach ($sanPhamGiamGias as $sanPhamGg)
                                     <div class="col-12 col-md-4 col-lg-2 mb-4">
                                         <div class="card product-card">

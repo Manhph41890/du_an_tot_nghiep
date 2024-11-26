b24d64c620722dbb33d7f7e0760ea6b979ed5fce
canmuidz <150830621+canmuidz@users.noreply.github.com>
2024-11-27 00:49:55 +0700

    show form lh

--- app/Http/Controllers/LienHeController.php
+++ app/Http/Controllers/LienHeController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\lien_he;
 use Illuminate\Http\Request;
 
 class LienHeController extends Controller
 {
     //
+    public function showForm(){
+        return view('client.lien_he');
+    }
+
 }

51962f08418bcda01a59cadf3ecae01c6dc89f2e
AnhDuc004 <ducanh29304@gmail.com>
2024-11-30 19:24:33 +0700

    .

--- app/Listeners/CreateWalletOnLogin.php
+++ app/Listeners/CreateWalletOnLogin.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\Login;
+use App\Models\Shipper;
 use App\Models\vi_nguoi_dung;
 use App\Models\Vishipper;
 use Illuminate\Auth\Events\Login as EventsLogin;
@@ -27,10 +28,12 @@ class CreateWalletOnLogin
         $user = $event->user; // Người dùng vừa đăng nhập
 
         if ($user->chuc_vu_id == 5) {
-            Vishipper::firstOrCreate(
-                ['shipper_id' => $user->id],
-                ['tong_tien' => 0]
-            );
+            if (Shipper::where('id', $user->id)->exists()) {
+                Vishipper::firstOrCreate(
+                    ['shipper_id' => $user->id],
+                    ['tong_tien' => 0]
+                );
+            }
         }
         // Kiểm tra và tạo ví người dùng nếu chưa tồn tại
         vi_nguoi_dung::firstOrCreate(

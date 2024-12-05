ff5a1b83c742c534d063f8d4d317ef97f64087df
luyenit <luyentvph41936@fpt.edu.vn>
2024-12-06 00:00:54 +0700

    code mở js

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -35,7 +35,7 @@
 
 <body data-menu-color="light" data-sidebar="default">
     {{-- biểu đồ  --}}
-    {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
